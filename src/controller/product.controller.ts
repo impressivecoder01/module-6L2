fa0583567c4106a0fc713645aa230510ca5e9940
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-11 02:02:44 +0600

    sevice.ts

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,19 +1,22 @@
 import type { IncomingMessage, ServerResponse } from "http";
+import { readProduct } from "../service/product.service";
 
 export const productController = (req :IncomingMessage , res: ServerResponse) => {
     const url = req.url
     const method = req.method
     
     if(url === "/products" && method === "GET"){
-        const products = [
-            {
-                id: 1,
-                name: 'Product-1'
+        // const products = [
+        //     {
+        //         id: 1,
+        //         name: 'Product-1'
 
-            }
-        ]
+        //     }
+        // ]
+        const products =  readProduct()
+
     res.writeHead(200, {"content-type" : "application/json"})
     res.end(JSON.stringify({message: "Products retrived successfull",
-     data:products}))
+     data: products}))
     }
 }
