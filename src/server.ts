f5551d65a01c81c30c492e3ea92bdb18394f1d6b
impressivecoder1 <rohanrahman251@gmail.com>
2026-05-10 23:45:36 +0600

    method

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import { createServer, IncomingMessage, Server } from "http";
 
 const server : Server = createServer((req : IncomingMessage, res)=>{
-    console.log(req);
+    // console.log(req.url);
+    // console.log(req.method);
+    const url = req.url
+    const method = req.method
+
+    if(url === '/' && method === 'GET'){
+        // console.log('this is root route');
+        res.writeHead(200, {"content-type" : "application/json"})
+        res.end(JSON.stringify({message: "This is root route"}))
+    }
+    else if(url?.startsWith('/products')){
+        res.writeHead(200, {"content-type" : "application/json"})
+        res.end(JSON.stringify({message: "This is products route"}))
+    }
+    else{
+        res.writeHead(404, {"content-type" : "application/json"})
+        res.end(JSON.stringify({message: "Route not found!"}))
+    }
 })
 
 server.listen(5000, ()=> {
